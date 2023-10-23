978b27c6dea676493b3520da663979fb4249904f
Yamil Castillo <yamilcs@gmail.com>
2023-10-22 23:59:11 -0300

    actualizando formato

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 import { GetFarmaciasTurno } from "../api/MiApi";
 
@@ -8,31 +8,27 @@ import Main from "./Main";
 import Footer from "./Footer";
 
 export const MiApi = () => {
-    const [data, setData] = useState([]);
-    const [originalData, setOriginalData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const farmacias = async () => {
-            setIsLoading(true);
-            const response = await GetFarmaciasTurno();
-            setData(response.data);
-            setOriginalData(response.data);
-            setIsLoading(false);
-        };
-        farmacias();
-    }, []);
+  useEffect(() => {
+    const farmacias = async () => {
+      setIsLoading(true);
+      const response = await GetFarmaciasTurno();
+      setData(response.data);
+      setOriginalData(response.data);
+      setIsLoading(false);
+    };
+    farmacias();
+  }, []);
 
   return (
-    <div className='container'>
-    <Header/>
-    <Buscador data={data}
-        setData={setData}
-        originalData={originalData}/>
-    <Main data={data}
-        originalData={originalData}
-        isLoading={isLoading}/>
-    <Footer/>
-</div>
-  )
-}
+    <div className="container">
+      <Header />
+      <Buscador data={data} setData={setData} originalData={originalData} />
+      <Main data={data} originalData={originalData} isLoading={isLoading} />
+      <Footer />
+    </div>
+  );
+};
